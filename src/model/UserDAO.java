14e49ff8259d35d53d74e24e29f091769cdefb3d
RavichandrenHakshan <ravichandrenhakshan200312@gmail.com>
2026-08-29 22:06:05 +0530

    Added LoginController with validation logic

--- src/model/UserDAO.java
+++ src/model/UserDAO.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Ravic
- */
+import util.DBconnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class UserDAO {
-    
+
+    public User authenticateUser(String username, String password) {
+        User loggedInUser = null;
+
+        try {
+            Connection con = DBconnection.getConnection();
+            
+            String sql = "SELECT * FROM users WHERE username=? AND password=?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            
+            pst.setString(1, username);
+            pst.setString(2, password);
+            
+            ResultSet rs = pst.executeQuery();
+            
+
+            if (rs.next()) {
+                loggedInUser = new User();
+                loggedInUser.setId(rs.getInt("id"));
+                loggedInUser.setUsername(rs.getString("username"));
+                loggedInUser.setRole(rs.getString("role"));
+                
+                System.out.println("User Authenticated. Role: " + loggedInUser.getRole());
+            }
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        return loggedInUser;
+    }
 }
--- src/util/DBconnection.java
+++ src/util/DBconnection.java
@@ -8,10 +8,8 @@ public class DBconnection {
     public static Connection getConnection(){
         Connection con = null;
         try {
-            // This matches the 8.x driver syntax your lecturer used
             Class.forName("com.mysql.cj.jdbc.Driver");
 
-            // Connecting to the WAMP database we discussed
             con = DriverManager.getConnection(
             "jdbc:mysql://localhost:3306/sunrise_dental_db",
             "root",
